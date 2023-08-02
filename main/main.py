3088f52c01408497fdf0bb475c8d02d53665e94e
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-08-03 03:24:18 +0600

    separating functionlities to several classes

--- main/main.py
+++ main/main.py
@@ -1,7 +1,18 @@
+from product_parser import ProductParser
 from saver import Saver
 
-URL = 'https://zakup.sk.kz/eprocsearch/api/external/4dv3rts/filter?size=10&page=0&sort=lastModifiedDate,desc'
-FILE_PATH = '../items.json'
+from formatter import format_data_to_tuple
 
-a = Saver(URL, FILE_PATH)
-a.save_to_database()
+URL = 'https://zakup.sk.kz/eprocsearch/api/external/4dv3rts/filter?size=10&page=0&sort=lastModifiedDate,desc'
+FILE_NAME = 'items.db'
+
+parser = ProductParser(URL)
+saver = Saver(FILE_NAME)
+
+data_list = []
+for item in parser.parse_items():
+    data_list.append(format_data_to_tuple(item))
+
+saver.save_to_database(data_list)
+
+
